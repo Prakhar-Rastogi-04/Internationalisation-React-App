29fc392c7b729df41140360b4dba07ccb8a3de7e
Prakhar-Rastogi-04 <35484284+Prakhar-Rastogi-04@users.noreply.github.com>
2021-03-25 17:11:19 +0530

    Add files via upload

---
+++ src/i18n/messages/fr-CA.js
@@ -0,0 +1,9 @@
+import { LOCALES } from "../locales"
+
+
+export default {
+    [LOCALES.FRENCH] :{
+        'welcome' : 'Bienvenue, {name}',
+        'copyright':'Créé et géré par {path} Tous droits réservés.'
+    }
+}
